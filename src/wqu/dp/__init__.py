7c81fd04c30c4e44998171cd248f0ef27d954b1f
Azat <8280770+azataiot@users.noreply.github.com>
2025-04-15 23:03:09 +0300

    feat: add Geometric Brownian Motion (GBM) class for stock price simulation and update version

--- src/wqu/dp/__init__.py
+++ src/wqu/dp/__init__.py
@@ -2,5 +2,6 @@
 
 from .utils import binomial_put_from_call as put_from_call
 from .binomial import BinomialTree
+from .gbm import GBM
 
-__all__ = ["put_from_call", "BinomialTree"]
+__all__ = ["put_from_call", "BinomialTree", "GBM"]
