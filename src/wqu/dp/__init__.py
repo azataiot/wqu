ecce7cba4bcb837bcc21e70b67bc9a756817d8f0
Azat <8280770+azataiot@users.noreply.github.com>
2025-04-15 23:28:37 +0300

    feat: update version to 0.1.12 and add Black-Scholes to module exports

--- src/wqu/dp/__init__.py
+++ src/wqu/dp/__init__.py
@@ -3,5 +3,6 @@
 from .utils import binomial_put_from_call as put_from_call
 from .binomial import BinomialTree
 from .gbm import GBM
+from .black_scholes import  BlackScholes
 
-__all__ = ["put_from_call", "BinomialTree", "GBM"]
+__all__ = ["put_from_call", "BinomialTree", "GBM","BlackScholes"]
